e79337139b5b5361b475e3bed3f3029b09972ce3
Sidi <sidimed.sidimed.147@gmail.com>
2024-10-15 11:06:26 +0200

    dossier fini

---
+++ wilders.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+console.log(chalk.bgCyan.bold('moi'));
+console.log(chalk.black.bold('lui'));
+console.log(chalk.red.bold('lui 2'));
+console.log(chalk.bgYellow.bold('lui 3'));
